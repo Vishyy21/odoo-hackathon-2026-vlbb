b63e9bef0ee699f417aed8ecb116294fe4234e28
Lakshya Dogra <lakshyadogra05@gmail.com>
2026-07-12 12:35:38 +0530

    feat(database): add users table

--- database/schema.sql
+++ database/schema.sql
@@ -11,3 +11,19 @@ PRAGMA foreign_keys = ON;
 -- USERS
 -- =====================================================
 
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+    full_name TEXT NOT NULL,
+
+    email TEXT NOT NULL UNIQUE,
+
+    password_hash TEXT NOT NULL,
+
+    role TEXT NOT NULL
+        CHECK(role IN ('Admin', 'Manager')),
+
+    created_at TEXT DEFAULT CURRENT_TIMESTAMP,
+
+    updated_at TEXT DEFAULT CURRENT_TIMESTAMP
+);
